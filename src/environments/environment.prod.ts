6bdbd83ad11caa035b5dbdbcde2a11132d446825
ilham-m <iilmaulana04@gmail.com>
2021-09-16 14:35:44 +0700

    merubah environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true
 };
-export const baseUrl = 'office-api.nayakateknologi.com/public/api/';
-export const downloadUrl = 'office-api.nayakateknologi.com/public/';
+export const baseUrl = 'https://office-api.nayakateknologi.com/public/api/';
+export const downloadUrl = 'https://office-api.nayakateknologi.com/public/';
